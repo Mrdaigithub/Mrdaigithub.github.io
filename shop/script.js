2ac6b2854d1b9a8f36222757b8e7a061d813730c
Mrdaigithub <346139312@qq.com>
2015-10-26 21:09:12 +0800

    v1.51

--- shop/script.js
+++ shop/script.js
@@ -1,1 +1,19 @@
-$(document).ready(function(){$(".menuBtn").hover(function(){$(this).find("div").stop().fadeIn(400)},function(){$(this).find("div").stop().fadeOut(400)});$("#banner")});
+
+$(document).ready(function () {
+    //分辨率大于480px的下拉菜单
+    $('.menuBtn').hover(function () {
+        $(this).find('div').stop().fadeIn(400);
+    }, function () {
+        $(this).find('div').stop().fadeOut(400);
+    });
+
+    //分辨率小于480px的下拉菜单
+    $('#menuBtn').click(function () {
+        console.log('ok');
+        $(this).nextAll().toggle(400);
+        $(this).nextAll().children('div').remove(); //无奈之举~
+    })
+
+    //焦点轮播图
+    
+})
