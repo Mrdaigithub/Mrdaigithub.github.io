9a0e1994ef428f8cf1d8cfaa5594cc12756ecd06
Mrdaigithub <346139312@qq.com>
2016-01-30 23:42:18 +0800

    ummmm

--- resume/result/js/script.js
+++ resume/result/js/script.js
@@ -1,29 +1,27 @@
 (function () {
     "use strict";
-    var str = ['h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!','<br>',
-            'h','e','l','l','o',' ','w','o','r','l','d','!'],
-        space = '_',
+    var text = document.getElementById('text'),
+        str = 'hello world!&hello javascript and nodeJs!',
         count = 1,
-        text = document.getElementById('text');
-
-    function typewriter(){
-        text.innerHTML = '';
-        for(var i=0; i<count; i++){
-            if(i > str.length-1){
-                return 0;
-            }
-            text.innerHTML += str[i];
+        arr = [];
+    function typewrite(){
+        for(var i=0; i<str.length; i++){
+            arr.push(str[i]);
         }
-        text.innerHTML += space;
+        if(count > arr.length){
+            return 0;
+        }
+        text.innerHTML = '';
+        for(var j=0; j<count; j++){
+            if(/&/.test(arr[j])){
+                arr.splice(j,1);
+                text.innerHTML += '<br>';
+            }
+            text.innerHTML += arr[j];
+        }
+        text.innerHTML += '<span class="cursor">_</span>';
         count++;
-
-        setTimeout(typewriter,100);
+        setTimeout(typewrite,100);
     }
-    typewriter();
+    typewrite();
 })();
