ecd6dabe114739d9aa8bc01317ab10b4c6f1f94a
Mrdaigithub <346139312@qq.com>
2016-02-01 00:43:33 +0800

    small

--- resume/result/js/script.js
+++ resume/result/js/script.js
@@ -1,27 +1,60 @@
 (function () {
     "use strict";
     var text = document.getElementById('text'),
-        str = 'hello world!&hello javascript and nodeJs!',
-        count = 1,
+        str = 'press enter to continue&' +
+            '<span class="fontColorGreen">hello world!</span> ' +
+            '<span class="fontColorGreen">good night!</span>',
+        count = 0,
         arr = [];
+    for(var i=0; i<str.length; i++){
+        arr.push(str[i]);
+    }
     function typewrite(){
-        for(var i=0; i<str.length; i++){
-            arr.push(str[i]);
-        }
-        if(count > arr.length){
+        if(count > arr.length-1){
+            text.innerHTML += ' <span class="cursor">_</span>';
             return 0;
         }
-        text.innerHTML = '';
-        for(var j=0; j<count; j++){
-            if(/&/.test(arr[j])){
-                arr.splice(j,1);
-                text.innerHTML += '<br>';
-            }
-            text.innerHTML += arr[j];
+
+        if(/&/.test(arr[count])){
+            text.innerHTML += '<br>';
+        }else if(arr[count] === '<'){
+            var obj = writeHTML(count);
+            count = obj.count;
+            text.innerHTML += obj.label;
+            var timer = null,
+                fontCount = 0;
+            timer = setInterval(function () {
+                if(fontCount >= obj.content.length-1){
+                    clearInterval(timer);
+                }
+                text.getElementsByClassName('fontColorGreen')[text.getElementsByClassName('fontColorGreen').length-1].innerHTML +=
+                    obj.content[fontCount];
+                fontCount++;
+            },50);
+        }else{
+            text.innerHTML += arr[count];
         }
-        text.innerHTML += '<span class="cursor">_</span>';
+
         count++;
-        setTimeout(typewrite,100);
+        setTimeout(typewrite,50);
     }
     typewrite();
+    function writeHTML(start){
+        var obj = {
+                str : '',
+                count : start,
+                label: '',
+                content: ''
+        },
+            reg = /^<\w+(\s+\w+=('|")(\s*\w+\s*)*('|"))*>.*<\/\w+>/;
+        obj.str += str[obj.count];
+        while(!(reg.test(obj.str))){
+            obj.count++;
+            obj.str += str[obj.count];
+        }
+        obj.content = obj.str.match(/>.*</)[0].replace(/<|>/g,'');
+        var regLabel = new RegExp(obj.content);
+        obj.label = obj.str.replace(regLabel,'');
+        return obj;
+    }
 })();
