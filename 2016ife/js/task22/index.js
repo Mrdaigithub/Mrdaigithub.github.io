92bab86d583fcfcec7dc9f046981083fb7fe8259
Mrdaigithub <346139312@qq.com>
2016-04-07 22:35:14 +0800

    doing task22

--- 2016ife/js/task22/index.js
+++ 2016ife/js/task22/index.js
@@ -1,3 +1,74 @@
 (function (WIN, DOC) {
-    
-})(window,document)
+
+    /**
+     * 要插入二叉树的节点类
+     */
+    class Node{
+        constructor(data,left,right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+        showData(){
+            return this.data;
+        }
+    }
+
+
+    /**
+     * 二叉树类
+     */
+    class BST{
+        constructor(){
+            this.root = null;
+        }
+        insert(data){
+            let n = new Node(data,null,null);
+
+            if (!this.root){
+                //如果是空的二叉树
+                this.root = n;
+            }else{
+                //如果不是空的二叉树
+                let current = this.root,
+                    parents = null;
+
+                while (true){
+                    if (data < current.data){
+                        //append leftChild
+                        current.left = current;
+                        if (!current){
+                            parents.left = n;
+                            console.log(parents);
+                            break;
+                        }
+                    }else{
+                        //append leftChild
+                        current.right = current;
+                        if (!current){
+                            parents.right = n;
+                            console.log(parents);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        preOrder(){
+
+        }
+        inOrder(){
+
+        }
+        posOrder(){
+
+        }
+        render(){
+
+        }
+    }
+
+    let a = new BST();
+    a.insert(5);
+    a.insert(3);
+})(window,document);
