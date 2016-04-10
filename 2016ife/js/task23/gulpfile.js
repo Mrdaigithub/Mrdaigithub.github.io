60c178421d666497f87f47e9df2b8e0af8504775
Mrdaigithub <346139312@qq.com>
2016-04-10 16:51:01 +0800

    start task23

---
+++ 2016ife/js/task23/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('babel', () => {
+    return gulp.src('index.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default',()=>{
+    gulp.watch('./index.js',['babel'],()=>{
+        console.log('babel working!');
+    })
+});
