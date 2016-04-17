dc0a5ca7adca601bbed32fe3b82a54c9356829b8
Mrdaigithub <346139312@qq.com>
2016-04-17 20:31:39 +0800

    complete task25

--- 2016ife/js/task25/dist/index.js
+++ 2016ife/js/task25/dist/index.js
@@ -1,58 +1,145 @@
 'use strict';
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function (WIN, DOC) {
-    var Data = function () {
-        function Data(data) {
-            _classCallCheck(this, Data);
 
-            this.data = data;
-            this.current = null;
+    var treeList = DOC.querySelector('.box>ul'),
+        searchBtn = DOC.querySelector('#searchBtn');
+
+    var Tree = function () {
+        function Tree(list) {
+            _classCallCheck(this, Tree);
+
+            this.list = list;
+            this.icon = '<span class="icon hide"> - </span>';
+            this.operate = '<div class="operate"><button class="add-btn">Add</button>\n<button class="rm-btn">Remove</button></div>';
         }
 
-        _createClass(Data, [{
-            key: 'addData',
-            value: function addData() {}
+        _createClass(Tree, [{
+            key: 'add',
+            value: function add() {
+                var _this = this;
+
+                treeList.addEventListener('click', function (event) {
+                    var e = WIN.event || event;
+                    if (e.target.className === 'add-btn') {
+                        var addVal = prompt('你想加点什么？');
+                        e.target.parentNode.parentNode.className = 'bold';
+                        console.log(e.target.parentNode.parentNode.getElementsByTagName('span'));
+                        e.target.parentNode.parentNode.getElementsByTagName('span')[0].className = 'icon lineShow';
+                        console.log(e.target.parentNode.parentNode);
+                        e.target.parentNode.parentNode.nextElementSibling.innerHTML += '<li><div>' + addVal + _this.icon + _this.operate + '</div><ul></ul></li>';
+                    }
+                }, false);
+            }
+
+            /**
+             * 删除节点
+             */
+
         }, {
-            key: 'rmData',
-            value: function rmData() {}
+            key: 'remove',
+            value: function remove() {
+                treeList.addEventListener('click', function (event) {
+                    var e = WIN.event || event;
+                    if (e.target.className === 'rm-btn') {
+                        e.target.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode);
+                    }
+                }, false);
+            }
+
+            /**
+             * 按照内容进行节点查找
+             * @param key
+             * @returns {boolean}
+             */
+
         }, {
-            key: 'findData',
-            value: function findData() {}
+            key: 'find',
+            value: function find(key) {
+                var stack = [];
+
+                for (var i = 0, leg = this.list.children.length; i < leg; i++) {
+                    stack.push(this.list.children[i]);
+                }
+
+                var _loop = function _loop() {
+                    if (!stack.length) {
+                        return {
+                            v: true
+                        };
+                    }
+
+                    if (stack[0].querySelector('ul').children) {
+                        for (var _i = 0, _leg = stack[0].querySelector('ul').children.length; _i < _leg; _i++) {
+                            stack.push(stack[0].querySelector('ul').children[_i]);
+                        }
+                    }
+
+                    var first = stack[0];
+                    if (first.firstElementChild.innerHTML.match(/\w+/)[0] === key.trim()) {
+                        first.style.backgroundColor = '#00ff00';
+                        setTimeout(function () {
+                            first.style.backgroundColor = '';
+                        }, 1000);
+                    }
+                    stack.shift();
+                };
+
+                while (true) {
+                    var _ret = _loop();
+
+                    if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
+                }
+            }
+        }, {
+            key: 'init',
+            value: function init() {
+                this.add();
+                this.remove();
+            }
         }]);
 
-        return Data;
+        return Tree;
     }();
 
-    var View = function View() {
-        _classCallCheck(this, View);
-    };
+    //节点的折叠与展开
 
-    var jsonData = [{
-        name: 'main',
-        child: [{
-            name: 'file1.1',
-            child: []
-        }, {
-            name: 'file1.2',
-            child: []
-        }]
-    }, {
-        name: 'file2',
-        child: [{
-            name: 'file2.1',
-            child: [{
-                name: 'file2.1.1',
-                child: []
-            }]
-        }, {
-            name: 'file2.2',
-            child: []
-        }]
-    }],
-        data = new Data(jsonData);
-    data.show();
+
+    treeList.addEventListener('click', function (event) {
+        var e = WIN.event || event;
+        if (e.target.nodeName === 'DIV' && e.target.className === 'bold') {
+            var list = e.target.nextElementSibling;
+            if (list.className === 'hide') {
+                list.className = '';
+                e.target.querySelector('span').innerHTML = ' - ';
+            } else {
+                list.className = 'hide';
+                e.target.querySelector('span').innerHTML = ' + ';
+            }
+        } else if (e.target.nodeName === 'SPAN') {
+            var _list = e.target.parentNode.nextElementSibling;
+            console.log(_list);
+            if (_list.className === 'hide') {
+                _list.className = 'show';
+                e.target.innerHTML = ' - ';
+            } else {
+                _list.className = 'hide';
+                e.target.innerHTML = ' + ';
+            }
+        }
+    }, false);
+
+    var a = new Tree(treeList);
+    a.init();
+
+    searchBtn.addEventListener('click', function (event) {
+        var e = WIN.event || event;
+        a.find(e.target.previousElementSibling.value);
+    }, false);
 })(window, document);
--- 2016ife/js/task25/index.js
+++ 2016ife/js/task25/index.js
@@ -1,64 +1,117 @@
 (function (WIN, DOC) {
 
-    class Data{
-        constructor(data){
-            this.data = data;
-            this.current = null;
+    let treeList = DOC.querySelector('.box>ul'),
+        searchBtn = DOC.querySelector('#searchBtn');
+
+    class Tree{
+        constructor(list){
+            this.list = list;
+            this.icon = '<span class="icon hide"> - </span>';
+            this.operate = '<div class="operate"><button class="add-btn">Add</button>\n<button class="rm-btn">Remove</button></div>';
         }
 
-        addData(){
-            
+        add(){
+            treeList.addEventListener('click',(event)=>{
+                let e = WIN.event || event;
+                if (e.target.className === 'add-btn'){
+                    let addVal = prompt('你想加点什么？');
+                    e.target.parentNode.parentNode.className = 'bold';
+                    console.log(e.target.parentNode.parentNode.getElementsByTagName('span'));
+                    e.target.parentNode.parentNode.getElementsByTagName('span')[0].className = 'icon lineShow';
+                    console.log(e.target.parentNode.parentNode);
+                    e.target.parentNode.parentNode.nextElementSibling.innerHTML += '<li><div>' + addVal + this.icon + this.operate + '</div><ul></ul></li>';
+                }
+            },false)
         }
 
-        rmData(){
-
+        /**
+         * 删除节点
+         */
+        remove(){
+            treeList.addEventListener('click',(event)=>{
+                let e = WIN.event || event;
+                if (e.target.className === 'rm-btn'){
+                    e.target.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode);
+                }
+            },false)
         }
 
-        findData(){
+        /**
+         * 按照内容进行节点查找
+         * @param key
+         * @returns {boolean}
+         */
+        find(key){
+            let stack = [];
 
+            for (let i=0, leg=this.list.children.length; i<leg; i++){
+                stack.push(this.list.children[i]);
+            }
+
+            while (true){
+                if (!stack.length){
+                    return true;
+                }
+
+                if (stack[0].querySelector('ul').children){
+                    for (let i=0,leg=stack[0].querySelector('ul').children.length; i<leg; i++){
+                        stack.push(stack[0].querySelector('ul').children[i]);
+                    }
+                }
+
+                let first = stack[0];
+                if (first.firstElementChild.innerHTML.match(/\w+/)[0] === key.trim()){
+                    first.style.backgroundColor = '#00ff00';
+                    setTimeout(()=>{
+                        first.style.backgroundColor = '';
+                    },1000)
+                }
+                stack.shift();
+            }
         }
 
+        init(){
+            this.add();
+            this.remove();
+        }
     }
 
-    class View{
-
-    }
 
 
 
-    let jsonData = [
-        {
-            name:'main',
-            child:[
-                {
-                    name:'file1.1',
-                    child:[]
-                },
-                {
-                    name:'file1.2',
-                    child:[]
-                }
-            ]
-        },
-        {
-            name:'file2',
-            child:[
-                {
-                    name:'file2.1',
-                    child:[
-                        {
-                            name:'file2.1.1',
-                            child:[]
-                        }
-                    ]
-                },
-                {
-                    name:'file2.2',
-                    child:[]
-                }
-            ]
+    
+    //节点的折叠与展开
+    treeList.addEventListener('click',(event)=>{
+        let e = WIN.event || event;
+        if (e.target.nodeName === 'DIV' && e.target.className === 'bold'){
+            let list = e.target.nextElementSibling;
+            if (list.className === 'hide'){
+                list.className = '';
+                e.target.querySelector('span').innerHTML = ' - ';
+            }else{
+                list.className = 'hide';
+                e.target.querySelector('span').innerHTML = ' + ';
+            }
+        }else if(e.target.nodeName === 'SPAN'){
+            let list = e.target.parentNode.nextElementSibling;
+            console.log(list);
+            if (list.className === 'hide'){
+                list.className = 'show';
+                e.target.innerHTML = ' - ';
+            }else{
+                list.className = 'hide';
+                e.target.innerHTML = ' + ';
+            }
         }
-    ],
-        data = new Data(jsonData);
-    data.show();
+    },false);
+
+
+    let a = new Tree(treeList);
+    a.init();
+
+
+    searchBtn.addEventListener('click',(event)=>{
+        let e = WIN.event || event;
+        a.find(e.target.previousElementSibling.value);
+    },false)
 })(window,document);
