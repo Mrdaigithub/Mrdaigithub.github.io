bbaad367cf010cd7779359708ae37faa666911ff
Mrdaigithub <346139312@qq.com>
2016-05-13 22:45:09 +0800

    changed

---
+++ celecare/query/webpack.config.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+    entry: {
+        index: './src/js/index.js',
+        weather: './src/js/weather.js',
+        express: './src/js/express.js'
+    },
+
+    output: {
+        filename: './dist/[name].js' // 输出文件的名称
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader!autoprefixer-loader?browsers=last 2 versions'
+            }
+        ]
+    }
+};
